71cf99f89a5c226675b63e62db32bf0a90efff3f
Markus Kainu <markus.kainu@gmail.com>
2017-04-22 20:17:09 +0300

    shinyapp into single app

--- inst/shiny-examples/lisapp/app.R
+++ inst/shiny-examples/lisapp/app.R
@@ -22,7 +22,7 @@ names(numeric_vars) <- unique(meta[meta$class %in% "numeric",]$name)
 factor_vars <-        unique(meta[meta$class %in% "factor",]$code)
 names(factor_vars) <- unique(meta[meta$class %in% "factor",]$name)
 
-fluidPage(
+ui <- fluidPage(
 
   # Application title
   titlePanel("Tidymetadata package with Luxembourg Income Study data"),
@@ -45,3 +45,26 @@ fluidPage(
     )
   )
 )
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  output$distPlot <- renderPlot({
+
+    d$var <- d[[input$var_numeric]]
+    d$class <- label_data(d, variable.data = input$var_factor, into.factor=TRUE)
+
+
+    if (input$method == "mean") d %>% group_by(class) %>%
+      summarise(value = mean(var, na.rm=TRUE)) -> dat
+    if (input$method == "median") d %>% group_by(class) %>%
+      summarise(value = median(var, na.rm=TRUE)) -> dat
+
+    ggplot(dat, aes(x=class,y=value)) + geom_col()
+
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
+
